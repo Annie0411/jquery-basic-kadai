0acade47bd15f5b8eb5176a74ce3c701387c4a9b
Annie0411 <146961552+Annie0411@users.noreply.github.com>
2023-11-27 20:24:38 +0900

    kadai_010ディレクトリにファイルを再アップロード

--- kadai_010/summary.js
+++ kadai_010/summary.js
@@ -1,25 +1,21 @@
-//P要素の色変更 
 $(function(){
   $('#change-color').on('click',function(){
     $('#target').css('color','red');
   });
 });
 
-//P要素のテキスト変更 
 $(function(){
   $('#change-text').on('click',function(){
     $('#target').text('Hello!');
   });
 });
 
-//P要素をフェードアウト 
 $(function(){
   $('#fade-out').on('click',function(){
     $('#target').fadeOut();
   });
 });
 
-//P要素をフェードイン
 $(function(){
   $('#fade-in').on('click',function(){
     $('#target').fadeIn();
